7320864bc2b17c6b097a4506e8635bd353888c06
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-07-07 15:29:16 +0530

    create a table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import Login from './Components/Screens/Login/Login';
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Screens/Dashboard/Dashboard";
-// import Supplier from "./Components/Screens/Supplier/Supplier";
+import Supplier from "./Components/Screens/Supplier/Supplier";
+import CustomTableCompoent from './Components/CustomComponents/CustomTableCompoent/CustomTableCompoent';
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/" element={<Login />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           {/* <Route path="/" element={<Supplier/>}/>  */}
+           <Route path="/" element={<CustomTableCompoent/>}/> 
+
         </Routes>
-
       </Router>
-
-
     </div>
-
   );
 }
 
---
+++ src/Components/Screens/Supplier/Supplier.js
@@ -0,0 +1,8 @@
+function Supplier() {
+    return (
+         <div>
+            
+        </div>
+    );
+}
+export default Supplier;
