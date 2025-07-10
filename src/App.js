5f62b2759647023a670cc1147ed3680a7715b540
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-07-10 11:16:02 +0530

    created custmer screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import Login from './Components/Screens/Login/Login';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./Components/Screens/Dashboard/Dashboard";
 import ListOfProducts from './Components/Screens/Products/ListOfProducts';
-// import Supplier from "./Components/Screens/Supplier/Supplier";
-// import Supplier from "./Components/Screens/Supplier/Supplier";
+import Supplier from "./Components/Screens/Supplier/Supplier";
 import CustomTableCompoent from './Components/CustomComponents/CustomTableCompoent/CustomTableCompoent';
 import DropdownCompoent from './Components/CustomComponents/DropdownCompoent/DropdownCompoent';
-// import Customer from './Components/Screens/Customer/Customer';
-// import SellsBilling from './Components/Screens/SellsBilling/SellsBilling';
+import Customer from './Components/Screens/Customer/Customer';
 function App() {
   return (
     <div>
@@ -15,15 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route path='/' element={<ListOfProducts />} /> */}
-          {/* <Route path="/" element={<Supplier/>}/>  */}
-           {/* <Route path="/" element={<CustomTableCompoent/>}/>  */}
-           {/* <Route path="/" element={<DropdownCompoent/>}/>  */}
-
         </Routes>
       </Router>
     </div>
   );
 }
-
 export default App;
