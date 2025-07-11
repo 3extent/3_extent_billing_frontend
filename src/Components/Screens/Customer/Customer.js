4d6572d02209fbd5448d0d02f863d73287a78e99
ApekshaV123 <138289397+ApekshaV123@users.noreply.github.com>
2025-07-11 10:36:27 +0530

    created custmer screen (#6)

---
+++ src/Components/Screens/Customer/Customer.js
@@ -0,0 +1,77 @@
+import CustomTableCompoent from "../../CustomComponents/CustomTableCompoent/CustomTableCompoent";
+import DropdownCompoent from "../../CustomComponents/DropdownCompoent/DropdownCompoent";
+import InputComponent from "../../CustomComponents/InputComponent/InputComponent";
+export default function Customer() {
+    const headers = [
+        "No",
+        "Customer Name",
+        "Address",
+        "Contact No",
+        "GST No",
+        "State",
+        "Balance",
+        "Customer Type",
+        "Bank",
+        "Account No",
+        "IFSC",
+        "Rate",
+        "Credit Amount"
+    ];
+    const rows = [{
+        "No": "1",
+        "Customer Name": "Nikita Kadam",
+        "Address": "Pune",
+        "Contact No": "1234567765",
+        "GST No": "27AAAPL1234C1ZV",
+        "State": "Maharashtra",
+        "Balance": 0,
+        "Customer Type": "Regular",
+        "Bank": "HDFC",
+        "Account No": "1234567890",
+        "IFSC": "HDFC0001234",
+        "Rate": "18%",
+        "Credit Amount": 5000
+    },
+    {
+        "No": "2",
+        "Customer Name": "Nikita Kadam",
+        "Address": "Pune",
+        "Contact No": "1234567765",
+        "GST No": "27AAAPL1234C1ZV",
+        "State": "Maharashtra",
+        "Balance": 0,
+        "Customer Type": "Regular",
+        "Bank": "HDFC",
+        "Account No": "1234567890",
+        "IFSC": "HDFC0001234",
+        "Rate": "18%",
+        "Credit Amount": 5000
+    }
+    ];
+    const CostomerTypes = ['a', 'b'];
+    return (
+        <div className="w-full">
+            <div className="text-xl font-serif">List Of Customer Information</div>
+            <div className="flex items-center gap-4 ">
+                <InputComponent
+                    type="text"
+                    placeholder="Customer Name"
+                />
+                <InputComponent
+                    type="text"
+                    placeholder="Contact No"
+                />
+                <DropdownCompoent
+                    options={CostomerTypes}
+                    placeholder="Select Customer Type"
+                />
+            </div>
+            <div>
+                <CustomTableCompoent
+                    headers={headers}
+                    rows={rows}
+                />
+            </div>
+        </div>
+    );
+}
