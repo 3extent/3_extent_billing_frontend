1011f5011991af06f4578ec39b915678c350bb66
RK <rk.datasigno@gmail.com>
2025-08-14 15:59:16 +0530

    Changes in barcode

--- src/Components/Screens/Products/SingleProductStockIn.js
+++ src/Components/Screens/Products/SingleProductStockIn.js
@@ -1,28 +1,51 @@
 import React, { useRef } from 'react';
 import Barcode from 'react-barcode';
-import { useReactToPrint } from 'react-to-print';
-
 
 export default function SingleProductStockIn() {
-    const printRef = useRef();
-    const handlePrint = () => {
-        const printContents = printRef.current.innerHTML;
-        const win = window.open('', '', 'width=600,height=400');
-        win.document.write('<html><head><title>Print Barcode</title></head><body>');
-        win.document.write(printContents);
-        win.document.write('</body></html>');
-        win.document.close();
-        win.focus();
-        win.print();
-        win.close();
-    };
+  const printRef = useRef();
 
-    return (
-        <div>
-            <div ref={printRef}>
-                <Barcode value="123456789012" />
-            </div>
-            <button onClick={handlePrint}>Print Barcode</button>
-        </div>
-    );
+  const handlePrint = () => {
+    const html = printRef.current.innerHTML;
+    const win = window.open('', '_blank', 'width=600,height=400');
+    win.document.write(`
+      <html>
+        <head>
+          <title>Print Barcode</title>
+          <style>
+            @media print {
+              body * {
+                visibility: hidden;
+              }
+              #print-box, #print-box * {
+                visibility: visible;
+              }
+              #print-box {
+                position: absolute;
+                top: 0;
+                left: 0;
+              }
+              html, body {
+                height: initial !important;
+                overflow: initial !important;
+              }
+            }
+          </style>
+        </head>
+        <body onload="window.focus(); window.print();">
+          <div id="print-box">${html}</div>
+        </body>
+      </html>
+    `);
+    win.document.close();
+    win.onafterprint = () => win.close();
+  };
+
+  return (
+    <div>
+      <div ref={printRef}>
+        <Barcode value="123456789012" />
+      </div>
+      <button onClick={handlePrint}>Print Barcode</button>
+    </div>
+  );
 }
