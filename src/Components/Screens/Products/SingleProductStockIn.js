cfb6d2fd802c933e51036a32a01a01176d900672
RK <rk.datasigno@gmail.com>
2025-08-20 15:52:04 +0530

    Barcode

--- src/Components/Screens/Products/SingleProductStockIn.js
+++ src/Components/Screens/Products/SingleProductStockIn.js
@@ -25,45 +25,34 @@ const SingleProductStockIn = () => {
     const win = window.open('', '', 'height=600,width=800');
     win.document.write(`
     <html>
-
-<head>
-  <title>Print Barcode</title>
-  <style>
-    @media print {
-
-      html,
-      body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        width: 100%;
-        overflow: hidden;
-      }
-
-      #barcode-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        page-break-inside: avoid;
-        page-break-before: avoid;
-        page-break-after: avoid;
-      }
-
-      svg {
-        max-width: 90%;
-        height: auto;
-        page-break-inside: avoid;
-      }
-    }
-  </style>
-</head>
-
-<body>
-  ${printContents}
-</body>
-
-</html>
+      <head>
+        <title>Print Barcode</title>
+        <style>
+          @page {
+            size: A4;
+            margin: 0;
+          }
+          html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            overflow: hidden;
+          }
+          svg {
+            width: 80%;
+            height: auto;
+            page-break-inside: avoid;
+          }
+        </style>
+      </head>
+      <body>
+        ${printContents}
+      </body>
+    </html>
   `);
     win.document.close();
     win.focus();
