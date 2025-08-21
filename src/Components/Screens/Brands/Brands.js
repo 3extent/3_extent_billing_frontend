737d062574559c36427c5f879ac8dc7fcdcd3930
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-08-21 15:42:40 +0530

    fixes brand id on brand screen

--- src/Components/Screens/Brands/Brands.js
+++ src/Components/Screens/Brands/Brands.js
@@ -21,7 +21,7 @@ function Brands() {
         console.log('response: ', response);
         if (response.status === 200) {
             const brandsFormattedRows = response.data.map((brand, index) => ({
-                "No": index + 1,
+                "No": brand.id,
                 "Brand Name": brand.name
             }));
             setRows(brandsFormattedRows);
