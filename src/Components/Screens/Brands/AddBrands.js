6780dd4c3f6ff584e254e6d526eafc61311d294d
Nikita Kadam <kadamnik68@gmail.com>
2025-09-09 04:59:26 -0700

    working on adding validation on brands screen and add toast msg on brands screen

--- src/Components/Screens/Brands/AddBrands.js
+++ src/Components/Screens/Brands/AddBrands.js
@@ -6,29 +6,29 @@ import PrimaryButtonComponent from "../../CustomComponents/PrimaryButtonComponen
 import { useNavigate } from "react-router-dom";
 export default function AddBrands() {
     const navigate = useNavigate();
-      const handleBack = () => {
-    navigate(-1);
-};
+    const handleBack = () => {
+        navigate(-1);
+    };
     const [brandData, setBrandData] = useState({
         name: ""
     });
-    const [loading, setLoading] = useState(false); 
-     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
     // const handleInputChange = (event) => {
     //     const { name, value } = event.target;
     //     setBrandData({ ...brandData, [name]: value });
     //     setError("");
     // };
-     const handleInputChange = (event) => {
-    const { name, value } = event.target;
-const hasSpecialChar = /[^a-zA-Z ]/.test(value);
-if (hasSpecialChar) {
-      setError("Special characters not allow");
-    } else {
-      setError("");
-    }
-setBrandData({ ...brandData, [name]: value });
-  };
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        const hasSpecialChar = /[^a-zA-Z ]/.test(value);
+        if (hasSpecialChar) {
+            setError("Special characters not allow");
+        } else {
+            setError("");
+        }
+        setBrandData({ ...brandData, [name]: value });
+    };
     const addBrandCallback = (response) => {
         console.log('response: ', response);
         if (response.status === 200) {
@@ -41,22 +41,22 @@ setBrandData({ ...brandData, [name]: value });
     };
     const addBrand = () => {
         if (brandData.name.trim() === "") {
-        console.log("Please enter brand name");
-             setError("please Enter Brand Name"); 
+            console.log("Please enter brand name");
+            setError("please Enter Brand Name");
             return;
         }
         if (/[^a-zA-Z ]/.test(brandData.name)) {
-      setError("Special characters not allow");
-      return;
-    }
+            setError("Special characters not allow");
+            return;
+        }
 
-    setError("");
+        setError("");
         apiCall({
             method: "POST",
             url: "https://3-extent-billing-backend.vercel.app/api/brands",
             data: brandData,
             callback: addBrandCallback,
-             setLoading: setLoading,
+            setLoading: setLoading,
         });
     };
     return (
@@ -67,7 +67,7 @@ setBrandData({ ...brandData, [name]: value });
                 <div className="text-red-600 mt-1 ml-1">
                     {error}
                 </div>
-)}
+            )}
             <InputComponent
                 label="Brand Name"
                 type="text"
@@ -79,18 +79,18 @@ setBrandData({ ...brandData, [name]: value });
                 onChange={handleInputChange}
             />
             <div className="flex mt-2">
-             <PrimaryButtonComponent
-               label="Back"
-               icon="fa fa-arrow-left"
-               buttonClassName="mt-2 py-1 px-5 mr-10 text-xl font-bold"
-               onClick={handleBack}
-                            />
-            <PrimaryButtonComponent
-                label="Submit"
-                icon="fa fa-bookmark-o"
-                buttonClassName="mt-2 py-1 px-5 text-xl font-bold"
-                onClick={addBrand}
-            />
+                <PrimaryButtonComponent
+                    label="Back"
+                    icon="fa fa-arrow-left"
+                    buttonClassName="mt-2 py-1 px-5 mr-10 text-xl font-bold"
+                    onClick={handleBack}
+                />
+                <PrimaryButtonComponent
+                    label="Submit"
+                    icon="fa fa-bookmark-o"
+                    buttonClassName="mt-2 py-1 px-5 text-xl font-bold"
+                    onClick={addBrand}
+                />
             </div>
         </div>
     );
