156c3674862963160b005c1c4471b559f6b9dc34
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-09-11 20:25:15 +0530

    handled separte callback function call getcustomer on add customer screen

--- src/Components/Screens/Customer/AddCustomer.js
+++ src/Components/Screens/Customer/AddCustomer.js
@@ -89,106 +89,107 @@ function AddCustomer() {
             setLoading: setLoading,
         });
     }
+    const getCustomerDataCallback = ((response) => {
+        if (response.status === 200) {
+            setCustomerData({
+                name: response.data.name, address: response.data.address,
+                state: response.data.state, contact_number: response.data.contact_number, gst_number: response.data.gst_number,
+                pan_number: response.data.pan_number,
+            });
+        }else{ 
+            // error("Failed to fetch customer data");
+        }
 
-    const getCustomerData = () => {
-        apiCall({
-            method: "GET",
-            url: `https://3-extent-billing-backend.vercel.app/api/users/${customer_id}`,
-            data: {},
-            callback: (response) => {
-                if (response.status === 200) {
-                    setCustomerData({
-                        name: response.data.name, address: response.data.address,
-                        state: response.data.state, contact_number: response.data.contact_number, gst_number: response.data.gst_number,
-                        pan_number: response.data.pan_number,
-                    });
-                } else {
-                    // error("Failed to fetch customer data");
-                }
-            },
-            setLoading: setLoading
-        });
-    }
-    return (
-        <div>
-            {loading && <Spinner />}
-            <div className='text-xl font-serif mb-4'>{customer_id ? "Edit Customer" : "Add Customer"}</div>
-            <div className="grid grid-cols-2 gap-x-5 gap-y-2">
-                <InputComponent
-                    label="Customer Name"
-                    type="text"
-                    name="name"
-                    placeholder="Customer Name"
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.name}
-                    onChange={handleInputChange}
-                />
-                <InputComponent
-                    label="Address"
-                    type="text"
-                    name="address"
-                    placeholder="Address"
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.address}
-                    onChange={handleInputChange}
-                />
-                <InputComponent
-                    label="State"
-                    type="text"
-                    name="state"
-                    placeholder="State"
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.state}
-                    onChange={handleInputChange}
-                />
-                <InputComponent
-                    label="Contact No "
-                    type="text"
-                    name="contact_number"
-                    placeholder="Contact No "
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.contact_number}
-                    onChange={handleInputChange}
-                />
-                <InputComponent
-                    label="GST No."
-                    type="text"
-                    name="gst_number"
-                    placeholder="GST No."
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.gst_number}
-                    onChange={handleInputChange}
-                />
-                <InputComponent
-                    label="PAN No."
-                    type="text"
-                    name="pan_number"
-                    placeholder="PAN No."
-                    inputClassName="w-[80%]"
-                    labelClassName="font-serif font-bold"
-                    value={customerData.pan_number}
-                    onChange={handleInputChange}
-                />
+        })
+        const getCustomerData = () => {
+            apiCall({
+                method: "GET",
+                url: `https://3-extent-billing-backend.vercel.app/api/users/${customer_id}`,
+                data: {},
+                callback: getCustomerDataCallback,
+                setLoading: setLoading
+            });
+        }
+        return (
+            <div>
+                {loading && <Spinner />}
+                <div className='text-xl font-serif mb-4'>{customer_id ? "Edit Customer" : "Add Customer"}</div>
+                <div className="grid grid-cols-2 gap-x-5 gap-y-2">
+                    <InputComponent
+                        label="Customer Name"
+                        type="text"
+                        name="name"
+                        placeholder="Customer Name"
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.name}
+                        onChange={handleInputChange}
+                    />
+                    <InputComponent
+                        label="Address"
+                        type="text"
+                        name="address"
+                        placeholder="Address"
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.address}
+                        onChange={handleInputChange}
+                    />
+                    <InputComponent
+                        label="State"
+                        type="text"
+                        name="state"
+                        placeholder="State"
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.state}
+                        onChange={handleInputChange}
+                    />
+                    <InputComponent
+                        label="Contact No "
+                        type="text"
+                        name="contact_number"
+                        placeholder="Contact No "
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.contact_number}
+                        onChange={handleInputChange}
+                    />
+                    <InputComponent
+                        label="GST No."
+                        type="text"
+                        name="gst_number"
+                        placeholder="GST No."
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.gst_number}
+                        onChange={handleInputChange}
+                    />
+                    <InputComponent
+                        label="PAN No."
+                        type="text"
+                        name="pan_number"
+                        placeholder="PAN No."
+                        inputClassName="w-[80%]"
+                        labelClassName="font-serif font-bold"
+                        value={customerData.pan_number}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="mt-4 flex justify-center">
+                    <PrimaryButtonComponent
+                        label="Back"
+                        icon="fa fa-arrow-left"
+                        buttonClassName="mt-2 py-1 px-5 mr-10 text-xl font-bold"
+                        onClick={handleBack}
+                    />
+                    <PrimaryButtonComponent
+                        label="Save"
+                        icon="fa fa-save"
+                        buttonClassName="mt-2 py-1 px-5 text-xl font-bold"
+                        onClick={addCustomer}
+                    />
+                </div>
             </div>
-            <div className="mt-4 flex justify-center">
-                <PrimaryButtonComponent
-                    label="Back"
-                    icon="fa fa-arrow-left"
-                    buttonClassName="mt-2 py-1 px-5 mr-10 text-xl font-bold"
-                    onClick={handleBack}
-                />
-                <PrimaryButtonComponent
-                    label="Save"
-                    icon="fa fa-save"
-                    buttonClassName="mt-2 py-1 px-5 text-xl font-bold"
-                    onClick={addCustomer}
-                />
-            </div>
-        </div>
-    )
-} export default AddCustomer;
+        )
+    } export default AddCustomer;
