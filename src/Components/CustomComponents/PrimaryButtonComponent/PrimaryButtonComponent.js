01debd1b61598484b6f047f62e4648cd0dde6b43
Nikita Kadam <kadamnik68@gmail.com>
2025-09-12 03:56:14 -0700

    on models screen handle validation handle toast msg then add customer and add supplier screen handle validation and toast msg and contact number max length fix and customer and supplier screen max length fix on contact number

--- src/Components/CustomComponents/PrimaryButtonComponent/PrimaryButtonComponent.js
+++ src/Components/CustomComponents/PrimaryButtonComponent/PrimaryButtonComponent.js
@@ -1,9 +1,12 @@
-export default function PrimaryButtonComponent({ label, onClick, icon, buttonClassName = "", iconPosition = "left" }) {
+export default function PrimaryButtonComponent({ label, onClick, icon, buttonClassName = "", iconPosition = "left", error = "", }) {
     return (
-        <button onClick={onClick} className={` bg-slate-800 text-white rounded flex items-center justify-center gap-2 ${buttonClassName}`}>
-            {icon && iconPosition === "left" && <i className={icon} aria-hidden="true"></i>}
-            <span className="">{label}</span>
-            {icon && iconPosition === "right" && <i className={icon} aria-hidden="true"></i>}
-        </button>
+        <div>
+            <button onClick={onClick} className={` bg-slate-800 text-white rounded flex items-center justify-center gap-2 ${buttonClassName}`}>
+                {icon && iconPosition === "left" && <i className={icon} aria-hidden="true"></i>}
+                <span className="">{label}</span>
+                {icon && iconPosition === "right" && <i className={icon} aria-hidden="true"></i>}
+            </button>
+            {error && <div className="text-red-600 mt-1 text-sm">{error}</div>}
+        </div>
     );
 };
