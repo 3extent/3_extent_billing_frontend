a0a2ef9fddca1f46aa28949e3bda7fd7aa485883
codadhyay <codadhyay@gmail.com>
2025-09-15 22:49:39 +0530

    Spinner changes

--- src/Util/AxiosUtils.js
+++ src/Util/AxiosUtils.js
@@ -22,11 +22,9 @@ export const apiCall = async ({ method, url, data, callback, setLoading }) => {
     }
 };
 export const Spinner = () => (
-    <div className="flex justify-center items-center h-screen">
-        <div
-            className="animate-spin rounded-full
-                 h-8 w-8 border-4 border-gray-900
-                 border-t-transparent"
-        ></div>
+    <div className="relative inset-0 z-50 flex items-center justify-center h-screen">
+        <div className="relative w-10 h-10">
+            <div className="absolute inset-0 rounded-full border-4 border-gray-600 border-t-transparent animate-spin"></div>
+        </div>
     </div>
 );
