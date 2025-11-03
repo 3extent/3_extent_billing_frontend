fef998d1a5876af9ea037464d3a7044feb14deee
Nikita Kadam <kadamnik68@gmail.com>
2025-11-03 16:38:12 +0530

    no changes

--- src/Components/Screens/Brands/AddModels.js
+++ src/Components/Screens/Brands/AddModels.js
@@ -10,7 +10,7 @@ export default function AddModels() {
     const { model_id } = useParams();
     const handleBack = () => {
         navigate(-1);
-    }; 
+    };
     useEffect(() => {
         getBrandsAllData();
     }, [])
