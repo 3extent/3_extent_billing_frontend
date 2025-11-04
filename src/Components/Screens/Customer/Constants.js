50e25b16fe4ed0d06433247e48daefd4eeb6056d
Nikita Kadam <kadamnik68@gmail.com>
2025-11-04 17:31:03 +0530

    add firm name column on customers screen

--- src/Components/Screens/Customer/Constants.js
+++ src/Components/Screens/Customer/Constants.js
@@ -1,4 +1,4 @@
-export const CUSTOMER_COLOUMS = ["Customer Name", "Contact No", "Address", "State", "GST No","PAN No","Action"]
+export const CUSTOMER_COLOUMS = ["Customer Name", "Contact No", "Firm Name", "Address", "State", "GST No", "PAN No", "Action"]
 
 
 
