82eea28eabf2920efe9b04a6cebe63609842bbc6
Apeksha Vishwasrao <apekshavishwasrao2002@gmail.com>
2025-12-18 14:14:19 +0530

    handle supplier and customer and billhistroy screen added dropdown

--- src/Components/Screens/Supplier/Constants.js
+++ src/Components/Screens/Supplier/Constants.js
@@ -1,2 +1,2 @@
-export const SUPPLIER_COLUMNS = ["Supplier Name", "Contact No", "GST No", "State", "Balance", "Action"]
+export const SUPPLIER_COLUMNS = ["Supplier Name", "Contact No", "GST No", "State","Address","Contact No 2","Firm Name", "Balance", "Action"]
 export const SUPPLIER_TYPE_OPTIONS = ['A', 'B', 'Regular'];
